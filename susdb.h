2a8d808e5ba806546f4e58b5e703e293a67d6803
Johan Anderholm <johan.anderholm@gmail.com>
2012-03-12 16:27:51 +0100

    db interface

--- susdb.h
+++ susdb.h
@@ -3,6 +3,7 @@
 
 #include <ctime>
 #include <string>
+#include <map>
 
 namespace sus {
     struct Article {
@@ -10,17 +11,24 @@ namespace sus {
         string author;
         string body;
         unsigned long ident;
-        time_t timestamp;
     }
 
     struct Newsgroup {
+        string name;
         unsigned long ident;
     }
 
 
     class DatabaseInterface {
         public:
-            
+            // Create newsgroup with 
+            virtual List<Newsgroup> getNewsGroups() = 0;
+            virtual void createNewsGroup(string name) = 0;
+            virtual void deleteNewsGroup(unsigned long newsIdent) = 0;
+            virtual List<Article> getArticles(unsigned long newsIdent) = 0;
+            virtual void createArticle(unsigned long newsIdent) = 0;
+            virtual void deleteArticle(unsigned long artIdent) = 0;
+            virtual void getArticle(unsigned long newsIdent, unsigned long artIdent) = 0;
     }
 }
 
