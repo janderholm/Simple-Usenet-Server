d09ea3711cb0bf25f27e9949c824e823122ae5ed
Johan Anderholm <johan.anderholm@gmail.com>
2012-03-12 17:24:22 +0100

    first part

--- inmemorydb.cc
+++ inmemorydb.cc
@@ -1,6 +1,31 @@
 #include "inmemorydb.h"
+#include "vector"
+
 
 using namespace std;
 using namespace sus;
 
+// TODO: A more C++ish way of doing it.
+static newsident = 0;
 
+vector<Newsgroup>
+InMemoryDatabase::getNewsgroups()
+{
+    return vector<Newsgroup> v;
+}
+
+void
+InMemoryDatabase::createNewsgroup(string name)
+{
+    // TODO: string reference.
+    MemoryNewsgroup n;
+    n->name = name;
+    n->ident = newsident++;
+    newsgroups.push_back(n); 
+}
+
+void
+InMemoryDatabase::deleteNewsgroup(unsigned long news)
+{
+    
+}
