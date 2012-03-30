7bb810b4089609c30ee0ea34785b88c1f7de14ed
Johan Anderholm <johan.anderholm@gmail.com>
2012-03-31 00:09:58 +0200

    server listening.

--- makefile
+++ makefile
@@ -1,6 +1,14 @@
 
+all: server
+
+clean:
+	$(RM) server
+
+server:
+	g++ -Wall --std=c++0x server.cc inmemorydb.cc private/*.cc -o server
+
 
 dbtest:
-	g++ --std=c++0x dbtest.cc inmemorydb.cc -o dbtest
+	g++ -Wall --std=c++0x dbtest.cc inmemorydb.cc -o dbtest
 	./dbtest && echo "SUCCESS!" || echo "FAIL!"
-	rm dbtest
+	@rm dbtest
--- server.cc
+++ server.cc
@@ -1,21 +1,47 @@
 #include <iostream>
 #include <sstream>
-#include <sstream>
+
+#include "private/server.h"
+#include "private/connection.h"
+#include "private/connectionclosedexception.h"
 
 using namespace std;
+using namespace client_server;
+//using namespace client_server::Connection;
+//using namespace client_server::ConnectionClosedException;
 
 int
 main(int argc, const char *argv[])
 {
     if (argc != 2) {
-        cout << "Usage: " << argv[0] << " PORT" << endl;
+        cerr << "Usage: " << argv[0] << " PORT" << endl;
         return 1;
     }
 
     int port;
     istringstream(argv[1]) >> port;
 
-    cout << port << endl;
+    Server server(port);
+    if (!server.isReady()) {
+        cerr << "Server initialization error" << endl;
+        return 1;
+    }
+
+    while (true) {
+        Connection* connection = server.waitForActivity();
+        if (connection != nullptr) {
+            try {
+                cout << connection->read() << endl;
+            } catch (ConnectionClosedException&) {
+                server.deregisterConnection(connection);
+                delete connection;
+                cout << "Client closed connection" << endl;
+            }
+        } else {
+            server.registerConnection(new Connection);
+            cout << "New client connects" << endl;
+        }
+    }
 
     return 0;
 }
